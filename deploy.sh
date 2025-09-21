ed7903c8ed9acf47ab0a5ec0dc76db5e20dc0972
Leandro Laurenzette <140437550+Leandro-Plan@users.noreply.github.com>
2025-09-21 10:48:34 -0300

    reestruturando para 3 instancias e nginx via docker

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ echo "=> Etapa 2/4: Construindo a nova imagem 'financeiro-app:latest'..."
 docker compose build
 
 # 3. Recriar o contêiner com a nova imagem, se necessário
-echo "=> Etapa 3/4: Recriando o serviço 'webapp'..."
-docker compose up -d
+echo "=> Etapa 3/4: Recriando e escalando o serviço 'webapp' para 3 instâncias..."
+docker compose up -d --scale webapp=3 --no-recreate
 
 # 4. Limpar imagens Docker antigas e não utilizadas para economizar espaço
 echo "=> Etapa 4/4: Limpando imagens Docker antigas..."
